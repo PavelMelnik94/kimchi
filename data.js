c1899fb961c7d016050831968a773c9793019a95
Pavel Melnik <recyclesordie94@gmail.com>
2022-05-14 16:02:15 +0400

    inital commit

---
+++ data.js
@@ -0,0 +1,96 @@
+export const menu1 = [
+    {
+        title: 'SIDES',
+        options: [
+            {id: 1, title: 'хумус', price: '500' },
+            {id: 2, title: 'кимчи', price: '500' },
+            {id: 3, title: 'рис', price: '500' },
+            {id: 4, title: 'салат из кинзы', price: '500' },
+            {id: 5, title: 'израильский салат', price: '500' },
+            {id: 6, title: 'битые огурцы', price: '500' },
+            {id: 7, title: 'кимчи редис', price: '500' },
+            {id: 8, title: 'маринованное яйцо', price: '500' },
+            {id: 9, title: 'соленые огурцы', price: '200' },
+            {id: 10, title: 'соленые овощи', price: '200' },
+            {id: 11, title: 'лаффа', price: '100' },
+        ]
+    },
+    {
+        title: 'SAUCES',
+        options: [
+            {id: 1, title: 'схуг', price: '200' },
+            {id: 2, title: 'тхина', price: '200' },
+            {id: 3, title: 'соус с кунжутом и зеленым луком', price: '200' },
+        ]
+    },
+    {
+        title: 'DRINKS',
+        options: [
+            {id: 1, title: 'пиво (ararat, gyumri)', price: '800' },
+            {id: 2, title: 'пиво (dargett)', price: '1100' },
+            {id: 3, title: 'фруктовая водка', price: '800' },
+            {id: 4, title: 'сода', price: '600' },
+            {id: 5, title: 'тан', price: '600' },
+            {id: 6, title: 'сок', price: '600' },
+            {id: 7, title: 'кофе', price: '600' },
+            {id: 8, title: 'чай', price: '600' },
+            {id: 9, title: 'вода', price: '500' },
+        ]
+    },
+];
+
+
+export const menu2 = [
+    {
+        title: 'COMBOS',
+        options: [
+            {
+                id: 1,
+                title: 'израильское комбо',
+                price: '1200',
+                description: 'хумус,  солёные овощи, израильский салат, соусы схуг и тхина, зелень и специи',
+                additional: [
+                    {id: 1, title: 'баклажан', price: '+500'},
+                    {id: 2, title: 'курица', price: '+800'},
+                    {id: 3, title: 'говядина (басар бакар)', price: '+1300'},
+                ]
+            },
+
+            {
+                id: 2,
+                title: 'корейское комбо',
+                price: '1200',
+                description: 'рис, кимчи, кимчи редис, битые огурцы, маринованное яйца и соус с кунжутом и зеленым луком',
+                additional: [
+                    {id: 1, title: 'баклажан', price: '+600'},
+                    {id: 2, title: 'курица', price: '+800'},
+                    {id: 3, title: 'говядина (бульгоги)', price: '+1300'},
+                ]
+            },
+
+        ]
+    },
+]
+
+
+export const menu3 = [
+    {title: 'MAIN', options: [
+            {id: 1, title: 'баклажан по-корейски', description: 'Баклажан темпура с соусом соя-лук-кунжут', price: '1300'},
+            {id: 2, title: 'баклажан по-израильски', description: 'Печеный баклажан с тхиной, специальными специями, зеленью и гранатом', price: '1300'},
+            {id: 3, title: 'бульгоги', description: 'Жареное мясо, маренованное в груше, имбире, чесноке и корейских специях с рисом и кимчи', price: '2500'},
+            {id: 4, title: 'басар бакар', description: 'Говядина с соусом из мацуна и тхины, вместе со схугом, израильскими специями, зеленью и гранатом', price: '2500'},
+        ],},
+    {title: 'DESSERT', options: [
+            {
+                id: 1,
+                title: 'малаби',
+                description: 'Баклажан темпура с соусом соя-лук-кунжут',
+                price: '600'},
+            {
+                id: 1,
+                title: 'пуноппан',
+                description: 'Печеный баклажан с тхиной, специальными специями, зеленью и гранатом',
+                price: '600'},
+        ]
+    },
+]
---
+++ main.js
@@ -0,0 +1,205 @@
+import './style.scss'
+import {WiredCard} from 'wired-elements'
+import { menu1, menu2, menu3 } from './data'
+
+
+
+window.addEventListener('load', () => {
+    const storage = window.localStorage
+
+//карта открытие\закрытие
+    const openOrderBtn = document.getElementById('order-open');
+    const closeOrderBtn = document.querySelector('.order-map__close')
+    const orderMap = document.querySelector('.order-map');
+
+    openOrderBtn.addEventListener('click', () => {
+        menuList.style.visibility  = 'hidden';
+        contactsList.style.visibility  = 'hidden';
+
+        orderMap.style.visibility  = 'visible';
+
+    });
+
+    closeOrderBtn.addEventListener('click', () => {
+        orderMap.style.visibility  = 'hidden';
+    });
+
+//========//
+
+//контакты открытие\закрытие
+    const openContactsBtn = document.getElementById('contacts-open');
+    const closeContactsBtn = document.querySelector('.contacts-list__close');
+    const contactsList = document.querySelector('.contacts-list');
+
+    openContactsBtn.addEventListener('click', () => {
+        menuList.style.visibility  = 'hidden';
+        orderMap.style.visibility  = 'hidden';
+
+        contactsList.style.visibility  = 'visible';
+
+    });
+
+    closeContactsBtn.addEventListener('click', () => {
+        contactsList.style.visibility  = 'hidden';
+    });
+
+//==========//
+
+
+//меню  открытие\закрытие
+    const openMenuBtn = document.getElementById('menu-open');
+    const closeMenuBtn = document.querySelector('.menu-list__close');
+    const menuList = document.querySelector('.menu-list');
+
+    openMenuBtn.addEventListener('click', () => {
+        contactsList.style.visibility  = 'hidden';
+        orderMap.style.visibility  = 'hidden';
+
+        menuList.style.visibility  = 'visible';
+        // menuList.setAttribute('open', true);
+
+
+        //содержимое меню
+        const menuHolder = document.querySelector('.menu-list__content');
+
+        // menu1.map(el => console.log(el))
+
+        const menuCategoryTemplate = (title, menuItem) => {
+            return (
+                `<div class="menu-list__content-group">
+                <div class="menu-list__content-group--title">${title}</div>
+                <div class="menu-list__content-group__section">
+                    ${menuItem}
+                </div>
+            </div>`
+            )
+        }
+
+        const item = (label, price,) => {
+            return `
+        <div class="menu-list__content-group__section-item">
+<span class="menu-list__content-group__section-item--label"> ${label}</span>
+ <span class="menu-list__content-group__section-item--price"> ${price}</span>
+        </div>`
+        }
+
+
+
+        const menuArr = [];
+
+
+        const itemFromDescr = (label, price, description, additional) => {
+            return `
+        <div class="menu-list__content-group__section-item flex-column">
+<div class="flex-between"><span class="menu-list__content-group__section-item--label"> ${label}</span>
+ <span class="menu-list__content-group__section-item--price"> ${price}</span></div>
+ <div class="menu-list__content-group__section-item--description">${description}</div>
+ <div class="menu-list__content-group__section-item--additional ">
+ ${additional}
+
+</div>
+        </div>`
+        }
+
+        menu2.map(menuItem => {
+            const {options} = menuItem;
+
+            const additional = options
+                .map(({title, price,description, additional })=> {
+
+                    const obj = additional?.map(({title, price}) => {
+                        // console.log(addOpt)
+                        return `<div class="menu-list__content-group__section-item--additional-item flex-between ">
+<span>${title} </span>  <span>${price}</span>
+</div>`
+                    });
+
+                    return itemFromDescr(title, price, description, obj?.join(" ") )
+                })
+            // console.log(additional)
+            menuArr.push(menuCategoryTemplate(menuItem.title, additional.join(" ")))
+
+
+            return null;
+        });
+
+
+
+        menu1.map(menuItem => {
+            const {options} = menuItem;
+
+            const additional = options.map(option => item(option.title, option.price))
+            menuArr.push(menuCategoryTemplate(menuItem.title, additional.join(" ")))
+
+
+            return null;
+        });
+
+
+        menu3.map(menuItem => {
+            const {options} = menuItem;
+
+            const additional = options.map(option => item(option.title, option.price))
+            menuArr.push(menuCategoryTemplate(menuItem.title, additional.join(" ")))
+
+
+            return null;
+        });
+
+
+
+
+        // const renderAdditional = (additionalOpt) => {
+        //     const arr = additionalOpt.map(el => el);
+        //     return `
+        //
+        //     `
+        // }
+
+
+        // console.log(el.additional)
+
+
+        // console.log(menuArr)
+
+        const arrModify = menuArr.join(" ")
+        menuHolder.innerHTML = arrModify
+
+//=========//
+
+    })
+
+    closeMenuBtn.addEventListener('click', () => {
+        menuList.style.visibility  = 'hidden';
+    })
+//=================//
+
+
+// модалка по языку
+    const landModal = document.querySelector('.lang-modal');
+    const landRusBtn = document.querySelector('.lang-rus');
+    const landAMBtn = document.querySelector('.lang-am');
+
+    const setLanguage = (value) => { storage.setItem('lang', `${value}`) }
+    window.addEventListener('load', () => {
+        const isLang = storage.getItem('lang')
+        if (!isLang) landModal.setAttribute('open', true)
+    })
+
+    landRusBtn.addEventListener('click', () => {
+        setLanguage('rus')
+        landModal.removeAttribute('open')
+    })
+
+    landAMBtn.addEventListener('click', () => {
+        setLanguage('am')
+        landModal.removeAttribute('open')
+    })
+
+//===================//
+})
+
+
+
+
+
